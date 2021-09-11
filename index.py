2acd5d8d5167307276cc974963248d77f2375049
Ben <ben290996@gmail.com>
2021-09-11 16:19:18 +0100

    Initial commit - Minimal flask with site template

---
+++ index.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # return render_template('test.html', title='Hi There', username='Ben')
+    return render_template('index.html', name='Ben', job_role='Software Engineer')
+
+if __name__ == "__main__":
+    app.run()
