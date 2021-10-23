f71464e99e70afa3358effd75255cbdd4eb8839c
Ben2917 <ben290996@gmail.com>
2021-10-23 21:44:16 +0100

    Added placeholder logo

--- index.py
+++ index.py
@@ -6,21 +6,29 @@ from eldritchxboxarticles import EldritchXboxArticles
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("page_template.html", content="<h1>Welcome!</h1>")
 
 
-@app.route('/projects')
+@app.route('/eldritch_xbox')
 def projects():
-
     eldritchXboxArticles = EldritchXboxArticles()
     content = eldritchXboxArticles.GetArticlesContent()
     return render_template("page_template.html", content=content)
 
+
+@app.route('/about')
+def about():
+    return render_template("page_template.html", content="<h1>About!</h1>")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("page_template.html", content="<h1>Contact!</h1>")
 
 
 if __name__ == "__main__":
